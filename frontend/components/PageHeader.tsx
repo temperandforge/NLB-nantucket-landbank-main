c51f49562941e55a9c1731c1388fa08aa4e9ff47
Jill <jill@temperandforge.com>
2026-07-21 11:44:38 -0400

    Adjust page header styles for tablet and small desktop screen sizes.

--- frontend/components/PageHeader.tsx
+++ frontend/components/PageHeader.tsx
@@ -2,12 +2,12 @@ export default function PageHeader(props) {
     const title = props.title ?? '';
     const copy = props.copy ?? '';
     return (
-        <div className="container flex flex-wrap py-24 gap-x-20 gap-y-10">
+        <div className="container flex max-lg:flex-col py-24 gap-x-20 gap-y-10">
             {title &&
-                <h1 className="max-w-[616px] text-3xl md:text-6xl leading-[1.2] font-bold">{title}</h1>
+                <h1 className="flex-1 text-3xl md:text-5xl xl:text-6xl leading-[1.2] font-bold">{title}</h1>
             }
             {copy &&
-                <p className="max-w-[616px] text-lg">{copy}</p>
+                <p className="flex-1 text-lg">{copy}</p>
             }
         </div>
     )
